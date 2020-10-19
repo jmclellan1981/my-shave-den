a332518a194f098c1b25dbf9d13a111a596694fe
jmclellan1981 <jmclellan1981@gmail.com>
2020-10-18 22:30:29 -0400

    use environment variable for sendgrid key

--- src/main/java/com/myshaveden/registration/RegistrationListener.java
+++ src/main/java/com/myshaveden/registration/RegistrationListener.java
@@ -1,29 +1,61 @@
 package com.myshaveden.registration;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.myshaveden.services.AppUserService;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 @Component
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
   @Autowired
   AppUserService userService;
 
-  @Value("app.email.key")
+  @Value("app.sendgrid.key")
   private String emailKey;
 
   @Override
   public void onApplicationEvent(OnRegistrationCompleteEvent event) {
 
-    this.confirmRegistration(event);
+    try {
+      this.confirmRegistration(event);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
   }
 
-  private void confirmRegistration(OnRegistrationCompleteEvent event) {
+  private void confirmRegistration(OnRegistrationCompleteEvent event) throws IOException {
+    Email from = new Email("test@example.com");
+    String subject = "Hello World from the SendGrid Java Library!";
+    Email to = new Email("test@example.com");
+    Content content = new Content("text/plain", "Hello, Email!");
+    Mail mail = new Mail(from, subject, to, content);
 
+    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    Request request = new Request();
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint("mail/send");
+      request.setBody(mail.build());
+      Response response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      throw ex;
+    }
   }
 
 }
