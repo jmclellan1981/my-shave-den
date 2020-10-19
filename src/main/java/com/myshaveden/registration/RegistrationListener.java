659904e24d013b42d36d76dee7b4c9ba6b50812f
jmclellan1981 <jmclellan1981@gmail.com>
2020-10-18 22:19:51 -0400

    remove send grid implementation

--- src/main/java/com/myshaveden/registration/RegistrationListener.java
+++ src/main/java/com/myshaveden/registration/RegistrationListener.java
@@ -1,21 +1,11 @@
 package com.myshaveden.registration;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.myshaveden.services.AppUserService;
-import com.myshaveden.viewmodels.AppUserModel;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 
 @Component
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
@@ -27,34 +17,13 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
 
   @Override
   public void onApplicationEvent(OnRegistrationCompleteEvent event) {
-    try {
-      this.confirmRegistration(event);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+
+    this.confirmRegistration(event);
+
   }
 
-  private void confirmRegistration(OnRegistrationCompleteEvent event) throws IOException {
-    AppUserModel user = event.getUser();
-    // String token = UUID.randomUUID().toString();
-    String subject = "Test Email";
-    String message = "This is a test";
-    Email from = new Email("noreply@my-shave-den.herokuapp.com");
-    Email to = new Email(user.getEmail());
-    Content content = new Content("text/plain", message);
-    Mail mail = new Mail(from, subject, to, content);
-    SendGrid sg = new SendGrid(emailKey);
-    Request request = new Request();
-    try {
-      request.setMethod(Method.POST);
-      request.setEndpoint("mail/send");
-      request.setBody(mail.build());
-      Response response = sg.api(request);
+  private void confirmRegistration(OnRegistrationCompleteEvent event) {
 
-    } catch (IOException ex) {
-      throw ex;
-    }
   }
 
 }
