a079d478a9deb05a31412dadf6bd6eea53c00c32
jmclellan1981 <jmclellan1981@gmail.com>
2020-10-18 21:57:55 -0400

    Use SendGrid to send emails

--- src/main/java/com/myshaveden/registration/RegistrationListener.java
+++ src/main/java/com/myshaveden/registration/RegistrationListener.java
@@ -1,15 +1,22 @@
 package com.myshaveden.registration;
 
-import java.util.UUID;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 import com.myshaveden.services.AppUserService;
 import com.myshaveden.viewmodels.AppUserModel;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 @Component
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
@@ -17,24 +24,39 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
   AppUserService userService;
   @Autowired
   JavaMailSender mailSender;
+  @Value("app.email.key")
+  private String emailKey;
 
   @Override
   public void onApplicationEvent(OnRegistrationCompleteEvent event) {
-    this.confirmRegistration(event);
+    try {
+      this.confirmRegistration(event);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
-  private void confirmRegistration(OnRegistrationCompleteEvent event) {
+  private void confirmRegistration(OnRegistrationCompleteEvent event) throws IOException {
     AppUserModel user = event.getUser();
-    String token = UUID.randomUUID().toString();
-    String emailAddress = user.getEmail();
+    // String token = UUID.randomUUID().toString();
     String subject = "Test Email";
     String message = "This is a test";
-    SimpleMailMessage email = new SimpleMailMessage();
-    email.setTo(emailAddress);
-    email.setSubject(subject);
-    email.setText(message);
-    mailSender.send(email);
+    Email from = new Email("noreply@my-shave-den.herokuapp.com");
+    Email to = new Email(user.getEmail());
+    Content content = new Content("text/plain", message);
+    Mail mail = new Mail(from, subject, to, content);
+    SendGrid sg = new SendGrid(emailKey);
+    Request request = new Request();
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint("mail/send");
+      request.setBody(mail.build());
+      Response response = sg.api(request);
 
+    } catch (IOException ex) {
+      throw ex;
+    }
   }
 
 }
